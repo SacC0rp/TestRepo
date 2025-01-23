5f82bf2cd3aab91a9126e46e1b23bdf0210bbb1a
Maciej Salwin <138807865+MaciejSalwin@users.noreply.github.com>
2025-01-23 21:11:03 +0100

    Update Main.java

--- .idea/Main.java
+++ .idea/Main.java
@@ -2,6 +2,8 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Test2");
-        System.out.println("Test1");
+        for(int i = 0; i < 10; i++){
+            System.out.println("Test1");    
+        }
     }
 }
